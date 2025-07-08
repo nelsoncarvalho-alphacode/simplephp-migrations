14abc3531bec5145d5e7e00111fa8a79afc76556
nelsoncarvalhosf <nelsoncarvalhosf@gmail.com>
2025-07-08 11:47:58 -0300

    Release v1.2.5 - Estrutura encapsulada + validação via Validator

--- resources/cli/rollback.php
+++ resources/cli/rollback.php
@@ -1,6 +1,7 @@
 <?php
 
-require_once __DIR__ . '/../vendor/autoload.php';
+$projectRoot = realpath(__DIR__ . '/..');
+require_once $projectRoot . '/vendor/autoload.php';
 
 $env = getenv('CLI_ENV') ?: 'dev';
 $_SERVER['HTTP_HOST'] = $env;
